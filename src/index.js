158a61a51c3656a10bf8d9de58a308badc12c14f
aigulmndzh <60320408+aigulmndzh@users.noreply.github.com>
2020-03-22 18:10:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+var reverse = function(x) {
+  var xstring = x.toString();
+  var result = "";
+
+  if (xstring[0] === '-') {
+    result += '-';
+    xstring = xstring.slice(1);
+  } else if (xstring[xstring.length - 1] === '0') {
+    xstring = xstring.slice(0, xstring.lenth - 1);
+  }
+  for (var i = xstring.length - 1; i>= 0; i--) {
+    result += xstring[i];
+  }
+
+  return result;
 }
